847136bea4e7e4845f5362c47a882ec81488a796
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-10-21 20:08:29 -0300

    <>

--- src/app/loja/[slug]/page.tsx
+++ src/app/loja/[slug]/page.tsx
@@ -1,14 +1,27 @@
 import { redirect } from "next/navigation";
 import { prisma } from "@/src/lib/prisma";
 import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/src/lib/authOptions";
 
 export default async function Page({ params }: { params: { slug: string } }) {
+  const session = await getServerSession(authOptions);
+
   const product = await prisma.product.findFirst({
     where: { id: Number(params.slug) },
   });
 
   if (!product) {
     redirect("/loja");
+  }
+
+  async function handleAddtoCart() {
+    "use server";
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session?.user?.email!,
+      },
+    });
   }
 
   return (
@@ -26,7 +39,10 @@ export default async function Page({ params }: { params: { slug: string } }) {
           <div className="text-3xl">{product.nome}</div>
           <div className="text-2xl">{product.descricao}</div>
           <div>R${product.preco}</div>
-          <button className="bg-emerald-200 shadow-md rounded-md p-3">
+          <button
+            onClick={handleAddtoCart}
+            className="bg-emerald-200 shadow-md rounded-md p-3"
+          >
             Adicionar ao carrinho
           </button>
         </div>
