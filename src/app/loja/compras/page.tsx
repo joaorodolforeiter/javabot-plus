555d822483573ebc01c500274f90c4c8007f8fb3
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-11-27 21:42:50 -0300

    changed some things

--- src/app/loja/compras/page.tsx
+++ src/app/loja/compras/page.tsx
@@ -1,10 +1,14 @@
 import PurchaseCard from "@/src/components/PurchaseCard";
 import { authOptions } from "@/src/lib/authOptions";
 import { prisma } from "@/src/lib/prisma";
+import { SmileySad } from "@phosphor-icons/react/dist/ssr";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
+import colors from "tailwindcss/colors";
 
 export default async function page() {
   const session = await getServerSession(authOptions);
+
   const orders = await prisma.order.findMany({
     where: {
       user: {
@@ -15,6 +19,18 @@ export default async function page() {
       orderItems: { include: { product: true } },
     },
   });
+
+  if (orders.length === 0 || true) {
+    return (
+      <div className="flex flex-col justify-center items-center gap-3">
+        <SmileySad size={120} weight="fill" color={colors.slate[300]} />
+        <div>Carrinho vazio</div>
+        <Link href="/loja" className="shadow-md bg-emerald-100 rounded-md p-3">
+          Voltar para a loja
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-6">
